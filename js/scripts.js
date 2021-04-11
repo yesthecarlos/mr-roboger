b9b276aa4681ab44efbdf516889133a2af6f189d
yesthecarlos <carlosmendez86@gmail.com>
2021-04-11 14:36:58 -0700

    WIP: beepBoopRobo function pseudocode

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,12 @@
 
 // Business Logic
 
+const userInput = 
+//make an array containing each integer from 0 to userInput
+//except numbers that contain a 1 are replaced with "Beep!" (UNLESS the number contains a 2 or a 3),
+//numbers that contain a 2 are replaced with "Boop" (UNLESS the number contains a 3) and 
+//numbers that contain a 3 are replaced with "Won't you be my neighbor?"
+//
 beepBoopRobo ()
 
 
