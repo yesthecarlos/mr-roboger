8904d6c30ebc31c76d675f6226ca461b73584486
yesthecarlos <carlosmendez86@gmail.com>
2021-04-11 14:57:49 -0700

    WIP: loop pushes integers to an array

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,19 @@
 
 // Business Logic
 
-const userInput = 
+
 //make an array containing each integer from 0 to userInput
 //except numbers that contain a 1 are replaced with "Beep!" (UNLESS the number contains a 2 or a 3),
 //numbers that contain a 2 are replaced with "Boop" (UNLESS the number contains a 3) and 
 //numbers that contain a 3 are replaced with "Won't you be my neighbor?"
 //
-beepBoopRobo ()
+// beepBoopRobo ()
 
+const userInput = 10;
+let integerArray = [];
+for (let index = 0; index <= userInput; index += 1){
+  integerArray.push(index)
+}
+integerArray;
 
 // UI Logic
